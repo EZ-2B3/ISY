3bb6931c445d5618df352c0c53d92c1549c0cdb2
Mirco Baalmans <mircobaalmans@live.nl>
2022-11-08 10:27:08 +0100

    Board houd grote bij en heet Copy()
    Board heeft nu een int rows en int cols.
    Ook is de functie toegevoegd. Die een Board return.

--- src/Board.java
+++ src/Board.java
@@ -1,8 +1,13 @@
 public class Board { // class to create the board for the game (CLI)
 
     private String[][] board;
+    private int rows = 0;
+    private int cols = 0;
 
     public Board(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+
         board = new String[rows][cols]; // 2D array waar de board in wordt opgeslagen
         for (int i = 0; i < rows; i++) { // Loop voor elke rij
             for (int j = 0; j < cols; j++) { // Loop voor elke kolom
@@ -18,6 +23,19 @@ public class Board { // class to create the board for the game (CLI)
 
     public void changeBoard(int rows, int cols, String piece) {
         board[rows][cols] = piece; // Veranderd de waarde van de rows en cols naar de piece
+    }
+
+    public Board Copy(){
+        //Makes a copy of the board and returns a new instance
+        Board copy = new Board(this.rows,this.cols);
+
+        for (int i = 0; i<this.rows; i++){
+            for (int j = 0; j < this.cols; j++){
+                copy.changeBoard(i,j,this.board[i][j]);
+            }
+        }
+
+        return copy;
     }
 }
 
