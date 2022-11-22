60ea1726dbba26ca52ba5a165ac6f245a0e03444
Reinder Noordmans <reindernoordmans@icloud.com>
2022-11-22 21:29:22 +0100

    Onnodige prints weggehaald, myPiece en opponent piece & printBoard

--- src/Game.java
+++ src/Game.java
@@ -14,6 +14,8 @@ class Game implements ActionListener { // class to listen for messages from serv
     private boolean useAI;
     private boolean isMyTurn = false;
     private String players;
+    private String myPiece;
+    private String opponentPiece;
 
     public Game() {
         this.render = new Render(this);
@@ -46,6 +48,11 @@ class Game implements ActionListener { // class to listen for messages from serv
 //                        String player = split[4].replace(",", "").replace("\"", ""); kan later nog wel handig zijn
                             int move = Integer.parseInt(split[6].replace(",", "").replace("\"", ""));
 
+                            if (myPiece == null) {
+                                myPiece = "O";
+                                opponentPiece = "X";
+                            }
+
                             String playerIcon;
                             if (moves % 2 == 0) {
                                 playerIcon = "X";
@@ -54,6 +61,7 @@ class Game implements ActionListener { // class to listen for messages from serv
                             }
 
                             board.setBoard(move, playerIcon);
+                            board.printBoard();
                             moves++;
                             render.BoardRender(board.getBoard(), isMyTurn, opponent);
                             render.UpdateFrame(render.panelBoard);
@@ -73,6 +81,8 @@ class Game implements ActionListener { // class to listen for messages from serv
                     } else if (message.contains("SVR PLAYERLIST")) {
                         String[] split = message.split("SVR PLAYERLIST ");
                         players = split[1].replace("[", "").replace("]", "").replace("\"", "");
+                    } else if (message.contains("OK")) {
+
                     } else {
                         System.out.println(message);
                     }
@@ -90,7 +100,6 @@ class Game implements ActionListener { // class to listen for messages from serv
 
     private void OnAIChoice(String buttonText) {
         useAI = buttonText.contains("Yes");
-        System.out.println("AI: " + useAI);
         render.UpdateFrame(render.panelGameChoice);
     }
 
@@ -107,6 +116,9 @@ class Game implements ActionListener { // class to listen for messages from serv
     }
 
     private void OnGameOver(String result) {
+        myPiece = null;
+        opponentPiece = null;
+        moves = 0;
         render.GameOverRender(result, opponent);
     }
 
@@ -161,7 +173,6 @@ class Game implements ActionListener { // class to listen for messages from serv
         try {
             String message = Connection.in.readLine();
             if (message.equals("OK")) {
-                System.out.println("Login successful");
                 render.UpdateFrame(render.panelAIChoice);
             } else {
                 JOptionPane.showMessageDialog(render.frame, message);
@@ -215,6 +226,10 @@ class Game implements ActionListener { // class to listen for messages from serv
 
             case "Quit":
                 OnQuit();
+                break;
+
+            case "ChallengeBack":
+                render.UpdateFrame(render.panelGameChoice);
                 break;
             //TODO add all Event calls.
         }
