da1f976737235f9f9727186cd7ada88b9a3d31e8
michy <michael_g@live.nl>
2022-12-15 11:40:40 +0100

    added class connection

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
-public class Main implements ActionListener {
-
-    private Game game;
-
+public class Main{
     public static void main(String[] args) {
-//       TODO: de main functie
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
+        Connection connection = new Connection("145.33.225.170", 7789);
+        connection.sendMessage("Login Reinder");
+        while (true) {
+            System.out.println(connection.receiveMessage());
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
